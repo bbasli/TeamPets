3a0be2e32e59ed2960a4a50ccd0b728cabdeb25a
Buğra <bgrbasli@gmail.com>
2022-01-31 01:53:41 +0300

    Inject prisma service into donation to get donations and creating new donations

--- api/src/donations/donations.service.ts
+++ api/src/donations/donations.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'prisma/prisma.service';
 import { CreateDonationInput } from './dto/create-donation.input';
 import { UpdateDonationInput } from './dto/update-donation.input';
 
 @Injectable()
 export class DonationsService {
-  create(createDonationInput: CreateDonationInput) {
-    return 'This action adds a new donation';
+  constructor(private prisma: PrismaService) {}
+
+  create(createDonationInput: Prisma.DonationCreateInput) {
+    return this.prisma.donation.create({ data: createDonationInput });
   }
 
   findAll() {
-    return `This action returns all donations`;
+    return this.prisma.donation.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} donation`;
+  findOne(donationWhereUniqueInput: Prisma.DonationWhereUniqueInput) {
+    return this.prisma.donation.findUnique({
+      where: donationWhereUniqueInput,
+    });
   }
 
   update(id: number, updateDonationInput: UpdateDonationInput) {
