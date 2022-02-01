75fa68575abedeb1dc8198a0651dca45a9b5eabc
bbasli <bgrbasli@gmail.com>
2022-02-01 23:59:28 +0300

    Update the header of the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 function App() {
   return (
-    <h1 className="text-5xl text-blue-700 font-bold underline">
-      Hello world!
-    </h1>
+    <div className="py-12 bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-bas font-semibold tracking-wide uppercase">Transactions</h2>
+          <p className="mt-2 leading-8 tracking-tight uppercase text-indigo-600 sm:text-4xl">
+            <span>#team<span className="font-extrabold">pets</span></span>
+          </p>
+          <p className="mt-4 max-w-2xl text-lg text-gray-500 lg:mx-auto">
+            We did it! Now let’s keep going. Come back anytime you feel like removing some trash!
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
