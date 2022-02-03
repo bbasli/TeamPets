a4da94a0714d38b555271588bffe056fdd78477c
bbasli <bgrbasli@gmail.com>
2022-02-03 23:08:23 +0300

    Put new counter component with dummy number to test

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { Counter } from "./components/Counter";
+
 function App() {
   return (
     <div className="py-12 bg-white font-montserrat">
@@ -10,6 +12,7 @@ function App() {
           <p className="mt-4 max-w-2xl text-lg text-gray-500 lg:mx-auto">
             We did it! Now let’s keep going. Come back anytime you feel like removing some trash!
           </p>
+          <Counter from={0} to={12312314} />
         </div>
       </div>
     </div>
